e15d92ab79ab7d220b5b54f73a2f5a6d3f9910e6
unknown <ysfradana23@gmail.com>
2026-03-01 21:40:53 +0700

    Restore handle method with correct signature to fix method not found error

--- app/Console/Commands/ServeCommand.php
+++ app/Console/Commands/ServeCommand.php
@@ -8,7 +8,9 @@ use Illuminate\Support\Env;
 use Illuminate\Support\InteractsWithTime;
 use Illuminate\Support\Stringable;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
 use function Illuminate\Support\php_binary;
@@ -101,8 +103,12 @@ class ServeCommand extends Command
      * @param  \Symfony\Component\Console\Output\OutputInterface  $output
      * @return int
      */
-    protected function handle()
+    protected function handle(InputInterface $input, OutputInterface $output)
     {
+        $this->input = $input;
+        $this->output = $output;
+        $this->configureIO($input, $output);
+
         $this->host = $this->host();
         $this->port = $this->port();
         $this->portOffset = 0;
