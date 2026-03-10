735a569ae8d24a065ebd3d7309d7f316434eda22
unknown <ysfradana23@gmail.com>
2026-03-10 23:34:04 +0700

    Fix admin logout buttons to POST with CSRF

--- resources/views/admin/edit-layanan.blade.php
+++ resources/views/admin/edit-layanan.blade.php
@@ -49,10 +49,13 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#" class="flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">
-                        <svg class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"/></svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
-                    </a>
+                    <form method="POST" action="{{ route('admin.logout') }}">
+                        @csrf
+                        <button type="submit" class="w-full text-left flex items-center px-2 py-1.5 text-body rounded-base hover:bg-neutral-tertiary hover:text-fg-brand group">
+                            <svg class="shrink-0 w-5 h-5 transition duration-75 group-hover:text-fg-brand" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"/></svg>
+                            <span class="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
+                        </button>
+                    </form>
                 </li>
             </ul>
         </div>
